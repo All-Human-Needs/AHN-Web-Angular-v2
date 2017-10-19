3afbf5099328c3d818eae9696674027cc7f4f59c
gertMinnie <gertminnie3@gmail.com>
2017-10-19 10:43:16 +0200

    shtuffs

--- src/app/components/client/client-maps/map/map.component.ts
+++ src/app/components/client/client-maps/map/map.component.ts
@@ -1,3 +1,4 @@
+import { Business } from '../../../../models/business/business.class';
 import { BusinessService } from './../../../../services/business.service';
 
 import { Component, OnInit } from '@angular/core';
@@ -8,60 +9,84 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
-  constructor(private businessService:BusinessService) { 
- 
-  }
-  
-    ngOnInit() {
-      this.setCurrentPosition();
-    }
-  
-    
 
-  locations:location[]=[];
-  userLocation:location;
-  userLat:number;
-  userLng:number;
-  zoom;
+export class MapComponent implements OnInit {
+
+  locations: Business[]; //= this.businessService.getBusinesses();
+  userLocation: location = this.setCurrentPosition();
+  userLat: number;
+  userLng: number;
+  userName: String = "You are here";
+  zoom: number;
+
+  constructor(private businessService: BusinessService) {
+  }
+
+  ngOnInit() {
+    // this.setCurrentPosition();
+    // this.getBusinessMarkers();
+    // this.businessService.getLocations();
+
+    // this.getMapMarkers();
+    this.businessService.getBusinesses().subscribe(
+      response=>{
+        this.locations = response
+        console.table(response);
+      }
+    )
+    console.log(this.locations[1].capacity);
+  }
+
+  private getMapMarkers() {
+    // this.locations = this.businessService.getBusinesses();
+
+    // console.log(this.locations[1].capacity);
+  }
 
   private setCurrentPosition() {
-    var newMarker:location;
+    var newMarker: location;
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(position => {
-      newMarker = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-        name:"You Are Here",
-       }
-       this.userLat=newMarker.lat;
-       this.userLng=newMarker.lng;
-       this.locations.push(newMarker);
+        newMarker = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+          name: "You Are Here",
+        }
+
+        // this.userLocation = {
+        //   lat: position.coords.latitude,
+        //   lng: position.coords.longitude,
+        //   name: "You Are Here",
+        // }
+
+        this.userLat = newMarker.lat;
+        this.userLng = newMarker.lng;
+        // this.locations.push(this.userLocation);
       });
     }
+
+    return this.userLocation;
+
   }
 
-getLocations(){
-this.locations=this.businessService.getBusinesses()}
-
-// custom styles for removing all the markers that aren't ours
-customStyle = [
-  {
-    featureType: "poi",
-    elementType: "labels",
-    stylers: [{visibility:"off" }]
-  },
-  {
-    featureType: "transit",
-    elementType: "labels",
-    stylers: [{visibility:"off" }]
-  }
-];
+  // custom styles for removing all the markers that aren't ours
+  customStyle = [
+    {
+      featureType: "poi",
+      elementType: "labels",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "transit",
+      elementType: "labels",
+      stylers: [{ visibility: "off" }]
+    }
+  ];
 
 }
 
-interface location{
-  lat:number;
-  lng:number;
-  name:string;
+interface location {
+  lat: number;
+  lng: number;
+  name: string;
 }
--- src/app/services/business.service.ts
+++ src/app/services/business.service.ts
@@ -1,4 +1,7 @@
-import { Business } from "../models/business/business.class";
+import { Business } from './../models/business/business.class';
+
+import { element } from 'protractor';
+
 import { Observable } from "rxjs/Observable";
 import { Injectable } from "@angular/core";
 import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
@@ -6,31 +9,40 @@ import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
 @Injectable()
 export class BusinessService {
   businessRef: AngularFireList<any>;
-  businesses: Observable<any[]>;
+  businesses: Observable<Business[]>;
 
   constructor(private db: AngularFireDatabase) {
-
-  }
-
-  getBusinesses():any {
+    var data: Business[];
     this.businessRef = this.db.list("businesses");
     this.businesses = this.businessRef.snapshotChanges().map(changes => {
       return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
-    });
-  //   this.businesses.forEach(element => {
-  //       console.log(element);
-  //   });
+   });
+  }
+
+  getBusinesses():Observable<Business[]> {
+    return this.businesses;
+  //  console.log(data);
+  //   return data;
   }
 
   deleteBusiness(key?: string) {
     this.businessRef.remove(key);
   }
 
-  addBusiness(business:Business) {
+  addBusiness(business: Business) {
     this.businessRef.push(business);
   }
 
-  updateBusiness(key: string, business:Business) {
+  updateBusiness(key: string, business: Business) {
     this.businessRef.update(key, business);
   }
+
+  // getLocations() {
+  //   this.businesses.forEach(element => {
+  //     for (let i = 0; i < element.length; i++) {
+  //       console.log('Lat'+element[i].lat);
+  //       console.log('Lng'+element[i].lng);
+  //     }
+  //   });
+  // }
 }
