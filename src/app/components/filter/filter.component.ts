bd928d89e895e7f51dc0d75f39e040e31682a232
VuyoBase <basevuyo@gmail.com>
2017-10-24 01:44:23 -0700

    filters working.
    Got the filters checkbox working.

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.css']
 })
 export class FilterComponent implements OnInit {
+  filterargs = {displayFiltered: 'location'};
+  emergencyBuildings = [{title: '/'}, {title: '/'}, {title: '/'}, {title: '/'}];
+  restaurants = [{title: '/'}, {title: '/'}, {title: '/'}, {title: '/'}];
+  governmentBuildings = [{title: '/'}, {title: '/'}, {title: '/'}, {title: '/'}];
+  shops = [{title: '/'}, {title: '/'}, {title: '/'}, {title: '/'}];
+  library = [{ title: '/'}, {title: '/'}, {title: '/'}, {title: '/'}];
 
   constructor() { }
 
   ngOnInit() {
   }
+displayFiltered ( userpref: String) {
+   if (userpref === 'Emergency Buildings') {
+  console.log(this.emergencyBuildings);
+ } else {if (userpref === 'Restaurants') {
+   console.log(this.restaurants);
+ } else {if (userpref === 'Government Buildings') {
+   console.log(this.governmentBuildings);
+ } else {if (userpref === 'Shops') {
+  console.log(this.shops);
+ } else {if (userpref === 'Library') {
+  console.log(this.library);
+ }}
+ } } } }}
 
-}
+
