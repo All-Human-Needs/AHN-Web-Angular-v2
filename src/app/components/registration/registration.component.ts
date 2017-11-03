5b5f920c6266b7fa01faea2087cd2b7b0c23677c
K9-98 <k9idas@gmail.com>
2017-11-03 08:34:22 +0200

    got data from register form

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,3 +1,5 @@
+import { Stats } from '../../models/business/stats.class';
+import { AuthenticationService } from '../../services/authentication.service';
 import { Business } from '../../models/business/business.class';
 import { BusinessService } from '../../services/business.service';
 import { Component, OnInit } from '@angular/core';
@@ -10,17 +12,33 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor(private _businessService:BusinessService) { }
+  constructor(private _businessService:BusinessService,private _authenticationService:AuthenticationService) { }
+
+
   isBusiness:boolean = false;
+  //id:string;
+  //key:string;
+  businessName:string;
+  lati:number;
+  long:number;
+  category:string ="bank";
+  capacity:number =10;
+  //isActive:true;
+  //stats: Stats[];
+
+
   form = new FormGroup({
     userForm: new FormGroup({ 
       email: new FormControl('',[Validators.required,Validators.email]),
-      password: new FormControl('',[Validators.required,Validators.minLength(8)]),
+      password: new FormControl('',Validators.required),
       confirmPwrd:new FormControl('',[Validators.required,Validators.pattern('1234')]),
     }),
     businessForm: new FormGroup({
-      lat: new FormControl('',[Validators.required]),
-      lng: new FormControl('',[Validators.required])
+      name: new FormControl('',[Validators.required,Validators.minLength(3)]),
+      lat: new FormControl('',[Validators.required,Validators.pattern("^[0-9]+$")]),
+      lng: new FormControl('',[Validators.required,Validators.pattern("^[0-9]+$")]),
+      capacity:new FormControl(),
+      category: new FormControl()
     }),
     
   });
@@ -44,6 +62,9 @@ export class RegistrationComponent implements OnInit {
   get lng(){
     return this.form.get('businessForm.lng');
   }
+  get name(){
+    return this.form.get('businessForm.name');
+  }
 
   ngOnInit() {
   }
@@ -53,18 +74,22 @@ export class RegistrationComponent implements OnInit {
   }
 
   register(){
-    let business:Business = {
-      id:"string",
-      //key:string;
-      name:"string",
-      lat:0,
-      lng:0,
-      category:"string",
-      capacity:150,
-      isActive:true,
-      stats: [],
+    if(this.isBusiness){
+      let business:Business = {
+        id:"string",
+        //key:string;
+        name: this.businessName,
+        lat: this.lati,
+        lng: this.long,
+        category: this.category,
+        capacity: this.capacity,
+        isActive: true,
+        stats: [],
+      }
+      console.log(business);
+      //this._businessService.addBusiness(business);
     }
-    this._businessService.addBusiness(business);
+    
   }
 
   validForm(){
