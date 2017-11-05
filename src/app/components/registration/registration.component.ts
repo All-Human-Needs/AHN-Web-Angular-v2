229fbe2647da28988d2f6b8bd9738c4055a6afbf
K9-98 <k9idas@gmail.com>
2017-11-05 22:26:45 +0200

    added form validation for password conformation

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,9 +1,13 @@
+import { PasswordValidators } from '../../models/password.validators';
+import { UserService } from '../../services/user.service';
+import { preparseElement } from '@angular/compiler/src/template_parser/template_preparser';
 import { Stats } from '../../models/business/stats.class';
 import { AuthenticationService } from '../../services/authentication.service';
 import { Business } from '../../models/business/business.class';
 import { BusinessService } from '../../services/business.service';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-registration',
@@ -11,48 +15,78 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-
-  constructor(private _businessService:BusinessService,private _authenticationService:AuthenticationService) { }
-
-
+  emailAddress:string;
+  pwd:string;
   isBusiness:boolean = false;
   //id:string;
   //key:string;
   businessName:string;
-  lati:number;
-  long:number;
+  lati:string;
+  long:string;
   category:string ="bank";
   capacity:number =10;
   //isActive:true;
   //stats: Stats[];
 
+form:FormGroup
 
-  form = new FormGroup({
-    userForm: new FormGroup({ 
-      email: new FormControl('',[Validators.required,Validators.email]),
-      password: new FormControl('',Validators.required),
-      confirmPwrd:new FormControl('',[Validators.required,Validators.pattern('1234')]),
-    }),
-    businessForm: new FormGroup({
-      name: new FormControl('',[Validators.required,Validators.minLength(3)]),
-      lat: new FormControl('',[Validators.required,Validators.pattern("^[0-9]+$")]),
-      lng: new FormControl('',[Validators.required,Validators.pattern("^[0-9]+$")]),
-      capacity:new FormControl(),
-      category: new FormControl()
-    }),
+  constructor(private _businessService:BusinessService,private _authenticationService:AuthenticationService,private _userService:UserService,fb:FormBuilder) {
+    this.form = fb.group({
+      userForm : fb.group({
+        email:['',[Validators.required,Validators.email]],
+        passwordForm: fb.group({  
+        password:['',[Validators.required,Validators.minLength(8)]],
+        confirmPwrd:['',Validators.required]}, 
+        {
+          validator: PasswordValidators.passwordMatches
+        }),
+      },
+     ),
+      businessForm: fb.group({
+        name:['',[Validators.required,Validators.minLength(3)]],
+        lat:['',[Validators.required,Validators.pattern("^[0-9]+$")]],
+        lng:['',[Validators.required,Validators.pattern("^[0-9]+$")]],
+        capacity:[],
+        category:[]
+      })
+    })
+   }
+
+ 
+
+
+  // form = new FormGroup({
+  //   userForm: new FormGroup({ 
+  //     email: new FormControl('',[Validators.required,Validators.email]),
+  //     password: new FormControl('',[Validators.required,Validators.minLength(8)]),
+  //     confirmPwrd:new FormControl('',[Validators.required,Validators.pattern('1234')]),
+  //   },{
+  //     validator: PasswordValidators.passwordMatches
+  //   }),
+  //   businessForm: new FormGroup({
+  //     name: new FormControl('',[Validators.required,Validators.minLength(3)]),
+  //     lat: new FormControl('',[Validators.required,Validators.pattern("^[0-9]+$")]),
+  //     lng: new FormControl('',[Validators.required,Validators.pattern("^[0-9]+$")]),
+  //     capacity:new FormControl(),
+  //     category: new FormControl()
+  //   }),
     
-  });
+  // });
 
   get email(){
     return this.form.get('userForm.email');
   }
 
   get password(){
-    return this.form.get('userForm.password');
+    return this.form.get('userForm.passwordForm.password');
+  }
+
+  get passwordForm(){
+    return this.form.get('userForm.passwordForm');
   }
 
   get confirmPwrd(){
-    return this.form.get('userForm.confirmPwrd');
+    return this.form.get('userForm.passwordForm.confirmPwrd');
   }
 
   get lat(){
@@ -74,21 +108,29 @@ export class RegistrationComponent implements OnInit {
   }
 
   register(){
-    if(this.isBusiness){
+    let isBusiness;
+    if(this.isBusiness){ 
+       isBusiness=true;
+      let newDate = new Date().toString();
       let business:Business = {
-        id:"string",
-        //key:string;
+        id:"",
         name: this.businessName,
-        lat: this.lati,
-        lng: this.long,
+        lat: parseFloat(this.lati) ,
+        lng: parseFloat(this.long) ,
         category: this.category,
         capacity: this.capacity,
         isActive: true,
-        stats: [],
+        stats: [{pax:0,date:newDate}],
       }
-      console.log(business);
-      //this._businessService.addBusiness(business);
+      this._authenticationService.createUser(this.emailAddress,this.pwd,isBusiness,business);
+
+    }else{
+      isBusiness =false;
+      this._authenticationService.createUser(this.emailAddress,this.pwd,isBusiness);
     }
+ 
+
+    
     
   }
 
